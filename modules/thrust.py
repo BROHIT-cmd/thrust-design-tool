6b8ef37b227529c9c3fa48d332e101f640aac697
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-06 09:59:37 +0530

    Update thrust.py

--- modules/thrust.py
+++ modules/thrust.py
@@ -1,24 +1,39 @@
 import math
 
 def pipe_area(diameter_mm):
+
     d = diameter_mm / 1000
+
     return math.pi * d**2 / 4
 
-def bend_thrust(
-  **diameter_mm,
-    pressure_bar,
-  **angle_deg
-):
-    area = pipe_area**iameter_mm)
 
-    pressure = press**e_bar * 100
+def bend_thrust(
+        diameter_mm,
+        pressure_bar,
+        angle_deg):
+
+    area = pipe_area(diameter_mm)
+
+    pressure = pressure_bar * 100
 
     thrust = (
-     ** 2
+        2
         * pressure
-        * a**a
+        * area
         * math.sin(
-           **ath.radians(angle_deg/2)
-        **    )
+            math.radians(angle_deg / 2)
+        )
+    )
 
     return thrust
+
+
+def dead_end_thrust(
+        diameter_mm,
+        pressure_bar):
+
+    area = pipe_area(diameter_mm)
+
+    pressure = pressure_bar * 100
+
+    return pressure * area
